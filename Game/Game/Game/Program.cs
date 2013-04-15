4d20b341309913bf1f8991ec01533c851ace17d7
Timothy Veletta <timothy.veletta@gmail.com>
2013-04-15 13:11:41 +0800

    Added project and DigitalRune references

---
+++ Game/Game/Game/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using DigitalRune;
+
+namespace Game
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Necessary for using the functions available in the DigitalRune engine.
+            DigitalRune.Licensing.AddSerialNumber("tgCcAcuJg2I1Hs4BYfH2YgY9zwEiACNUaW1vdGh5IFZlbGV0dGEjMSMzI05vbkNvbW1lcmNpYWxAg7LDkM1RVAQDAfwPTE1CWShg8ydjAzN820/FDwY81X7iRsmnEZIi9Zr3Fz46IC9N7KhUL6OSReHCzBLn");
+
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
