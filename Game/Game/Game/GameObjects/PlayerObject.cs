e03c54e2bc3d7627ed9a5fdfdda6135d8bcb8999
Arran <arran_ford@live.com>
2013-04-24 23:26:30 +0800

    some game objects

---
+++ Game/Game/Game/GameObjects/PlayerObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DigitalRune.Game;
+using DigitalRune.Geometry;
+using DigitalRune.Geometry.Shapes;
+using DigitalRune.Mathematics.Algebra;
+using DigitalRune.Physics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ICT309Game.GameObjects
+{
+    class PlayerObject : GameObject
+    {
+
+        public PlayerObject()
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+
+        protected override void OnUpdate(TimeSpan deltaTime)
+        {
+            base.OnUpdate(deltaTime);
+        }
+
+
+    }
+}
