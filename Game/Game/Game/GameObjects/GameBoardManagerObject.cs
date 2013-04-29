92fb0b1d03db5d14dc79d86b8f89e261eb0717ca
Timothy Veletta <timothy.veletta@gmail.com>
2013-04-30 01:41:20 +0800

    Shows 2 squares distance from the place you clicked

--- Game/Game/Game/GameObjects/GameBoardManagerObject.cs
+++ Game/Game/Game/GameObjects/GameBoardManagerObject.cs
@@ -10,9 +10,18 @@ using DigitalRune.Game.Input;
 using DigitalRune.Geometry;
 using DigitalRune.Graphics;
 using Microsoft.Xna.Framework;
+using DigitalRune.Collections;
 
 namespace ICT309GameGame.GameObjects
 {
+    enum SquareData
+    {
+        BLOCKED = 0,
+        EMPTY = 1,
+        OCCUPIED = 2,
+        HIGHLIGHTED = 3,
+    };
+
     class GameBoardManagerObject:GameObject
     {
         private const float startPos = -114.5f;
@@ -20,8 +29,21 @@ namespace ICT309GameGame.GameObjects
 
         private RedSquareObject[,] redGameBoard = new RedSquareObject[10,10];
         private WhiteSquareObject[,] whiteGameBoard = new WhiteSquareObject[10, 10];
+        private BlueSquareObject[,] blueGameBoard = new BlueSquareObject[10, 10];
+
+        public SquareData[,] GameBoard = new SquareData[10, 10];
 
         public GameBoardManagerObject()
+        {
+            ResetBoard();
+
+            // TODO make the game board data load from an external file
+            // in the meantime heres this array
+
+           
+        }
+
+        protected override void  OnLoad()
         {
             var gameObjectService = ServiceLocator.Current.GetInstance<IGameObjectService>();
 
@@ -29,6 +51,10 @@ namespace ICT309GameGame.GameObjects
             {
                 for (int j = 0; j < redGameBoard.GetLength(1); j++)
                 {
+                    blueGameBoard[i, j] = new BlueSquareObject(new Vector3F(
+                                            gap * i + startPos,
+                                            0.0f,
+                                            gap * j + startPos));
                     redGameBoard[i, j] = new RedSquareObject(new Vector3F(
                                             gap * i + startPos,
                                             0.0f,
@@ -37,20 +63,48 @@ namespace ICT309GameGame.GameObjects
                                             gap * i + startPos,
                                             0.0f,
                                             gap * j + startPos));
-                    gameObjectService.Objects.Add(redGameBoard[i,j]);
+                    
+                    gameObjectService.Objects.Add(redGameBoard[i, j]);
+                    gameObjectService.Objects.Add(blueGameBoard[i, j]);
                     gameObjectService.Objects.Add(whiteGameBoard[i, j]);
                 }
             }
-
-
-            
         }
 
         protected override void OnUpdate(TimeSpan deltaTime)
         {
+            for (int i = 0; i < GameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < GameBoard.GetLength(1); j++)
+                {
+                    switch(GameBoard[i,j])
+                    {
+                        case SquareData.BLOCKED:
+                            redGameBoard[i, j].InUse = false;
+                            whiteGameBoard[i, j].InUse = false;
+                            blueGameBoard[i, j].InUse = false;
+                            break;
+                        case SquareData.EMPTY:
+                            redGameBoard[i, j].InUse = false;
+                            whiteGameBoard[i, j].InUse = true;
+                            blueGameBoard[i, j].InUse = false;
+                            break;
+                        case SquareData.HIGHLIGHTED:
+                            redGameBoard[i, j].InUse = true;
+                            whiteGameBoard[i, j].InUse = false;
+                            blueGameBoard[i, j].InUse = false;
+                            break;
+                        case SquareData.OCCUPIED:
+                            redGameBoard[i, j].InUse = false;
+                            whiteGameBoard[i, j].InUse = false;
+                            blueGameBoard[i, j].InUse = true;
+                            break;
+                    }
+                }
+            }
+
             var gameObjectService = ServiceLocator.Current.GetInstance<IGameObjectService>();
             var inputService = ServiceLocator.Current.GetInstance<IInputService>();
-
             CameraObject camera; 
             gameObjectService.Objects.TryGet("Camera", out camera);
 
@@ -60,8 +114,8 @@ namespace ICT309GameGame.GameObjects
             {
                 mousePos = camera.MousePosition;
 
-                int IndexI = (int)((mousePos.X - startPos + 12.75f) / gap);
-                int IndexJ = (int)((mousePos.Z - startPos + 12.75f) / gap);
+                int IndexI = (int)((mousePos.X - startPos + (gap / 2.0f)) / gap);
+                int IndexJ = (int)((mousePos.Z - startPos + (gap / 2.0f)) / gap);
 
                 if (IndexI >= 0 && IndexI <= 9 && IndexJ >= 0 && IndexJ <= 9)
                 {
@@ -70,38 +124,96 @@ namespace ICT309GameGame.GameObjects
                     Console.WriteLine("Index I = " + IndexI);
                     Console.WriteLine("Index J = " + IndexJ);
 
-                    redGameBoard[IndexI, IndexJ].InUse = true;
-                    whiteGameBoard[IndexI, IndexJ].InUse = false;
+                    ResetBoard();
+                    ShowMovementRange(IndexI, IndexJ, 2);
                 }
-            }
-
-            /**** Add debug rendering stuff here ****/
-#if DEBUG
-            var debugRenderer = ServiceLocator.Current.GetInstance<DebugRenderer>();
-
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    debugRenderer.DrawLine(new Vector3F(gap * i + startPos, 2.0f, gap * j + startPos),
-                        new Vector3F(gap * i + startPos + (114.5f), 2.0f, gap * j + startPos), Color.Red, false);
-                    debugRenderer.DrawLine(new Vector3F(gap * i + startPos, 2.0f, gap * j + startPos),
-                        new Vector3F(gap * i + startPos, 2.0f, gap * j + startPos + (114.5f)), Color.Red, false);
-                }
-            }
-#endif
+            } 
+           
 
             base.OnUpdate(deltaTime);
-        }
-
-        protected override void OnLoad()
-        {
-            base.OnLoad();
         }
 
         protected override void OnUnload()
         {
             base.OnUnload();
+        }
+
+        private void ResetBoard()
+        {
+            for (int i = 0; i < GameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < GameBoard.GetLength(1); j++)
+                {
+                    GameBoard[i, j] = SquareData.EMPTY;
+                }
+            }
+        }
+
+        public void ShowMovementRange(int x, int z, int range)
+        {
+            Console.WriteLine("ShowMovementRange");
+
+            if(!CheckRange(x) ||
+                !CheckRange(z) ||
+                !CheckRange(range))
+            {
+                return;
+            }
+
+            Console.WriteLine("ShowMovementRange");
+
+            List<Pair<int>> nodeList = new List<Pair<int>>();
+            nodeList.Add(new Pair<int>(x,z));
+
+            while (range > 0)
+            {
+                Console.WriteLine("In loop");
+                int count = nodeList.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    // UP
+                    if (CheckRange(nodeList[i].First + 1))
+                        nodeList.Add(new Pair<int>(nodeList[i].First + 1, nodeList[i].Second));
+
+                    // DOWN
+                    if (CheckRange(nodeList[i].First - 1))
+                        nodeList.Add(new Pair<int>(nodeList[i].First - 1, nodeList[i].Second));
+
+                    // LEFT
+                    if (CheckRange(nodeList[i].Second + 1))
+                        nodeList.Add(new Pair<int>(nodeList[i].First, nodeList[i].Second + 1));
+
+                    // RIGHT
+                    if (CheckRange(nodeList[i].Second - 1))
+                        nodeList.Add(new Pair<int>(nodeList[i].First, nodeList[i].Second - 1));
+                }
+
+                range--;
+            }
+
+            Console.WriteLine("Count =" + nodeList.Count);
+
+            for (int i = 0; i < nodeList.Count; i++)
+            {
+                if (CheckRange(nodeList[i].First) && CheckRange(nodeList[i].Second))
+                    // TODO 
+                    GameBoard[nodeList[i].First, nodeList[i].Second] = SquareData.HIGHLIGHTED;
+            }
+
+            GameBoard[x, z] = SquareData.OCCUPIED;
+        }
+
+        private bool CheckRange(int x)
+        {
+            if (x >= 10 || x < 0)
+            {
+                Console.WriteLine("CheckRange " + x + " false.");
+                return false;
+            }
+
+            Console.WriteLine("CheckRange " + x + " true.");
+            return true;
         }
     }
 }
